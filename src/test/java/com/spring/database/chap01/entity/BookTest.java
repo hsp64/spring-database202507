495acadcade356c31b77dbcc5a5e39eb472f8116
hsp64 <pphs604@gmail.com>
2025-07-22 12:43:55 +0900

    도서 테스트 클래스에 CRUD 테스트 메서드 추가 및 리팩토링

--- src/test/java/com/spring/database/chap01/entity/BookTest.java
+++ src/test/java/com/spring/database/chap01/entity/BookTest.java
@@ -8,22 +8,23 @@ import javax.sql.DataSource;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-@SpringBootTest // 의존 객체를 테스트시에 주입받기위한 설정
+@SpringBootTest   // 의존 객체를 테스트시에 주입받기위한 설정
 class BookTest {
 
-    @Autowired
+    @Autowired // 필드 주입
     private DataSource dataSource;
 
     @Test
     void insertTest() {
         // 책 한권의 데이터를 DB에 실제로 저장
-        // DB를 연동
-        try {
+        // DB를 연결
+        try (Connection conn = dataSource.getConnection()) {
             // 1. 데이터베이스 소켓 연결 - DB에 인증정보를 주고 확인받는 작업
-            Connection conn = dataSource.getConnection();
 
             // 2. SQL 작성
             String sql = """
@@ -44,7 +45,87 @@ class BookTest {
             // 5. SQL을 실행
             pstmt.executeUpdate();
 
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        /*finally {
+            // 데이터베이스 연결 해제
+            try {
+                if (conn != null) conn.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }*/
+    }
 
+    @Test
+    void updateTest() {
+        try (Connection conn = dataSource.getConnection()) {
+            String sql = """
+                    UPDATE books
+                    SET author = ?, isbn = ?
+                    WHERE id = ?
+                    """;
+            PreparedStatement pstmt = conn.prepareStatement(sql);
+            pstmt.setString(1, "오로라핑");
+            pstmt.setString(2, "Z999");
+            pstmt.setString(3, "5L");
+
+            pstmt.executeUpdate();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    void deleteTest() {
+        try (
+                Connection conn = dataSource.getConnection()) {
+            String sql = """
+                    DELETE FROM books
+                    WHERE id = ?
+                    """;
+            PreparedStatement pstmt = conn.prepareStatement(sql);
+            pstmt.setString(1, "2L");
+
+            pstmt.executeUpdate();
+        } catch (
+                Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    // 전체조회
+    @Test
+    void findAllTest() {
+        try (Connection conn = dataSource.getConnection()) {
+
+            String sql = """
+                    SELECT * FROM BOOKS
+                    WHERE title LIKE ?
+                    """;
+
+            PreparedStatement pstmt = conn.prepareStatement(sql);
+
+            pstmt.setString(1,"%노잼%");
+
+            // SQL 실행
+            ResultSet rs = pstmt.executeQuery();
+
+            while (rs.next()) {
+                String title = rs.getString("title");
+                String author = rs.getString("author");
+                String isbn = rs.getString("isbn");
+
+                System.out.println("title = " + title);
+                System.out.println("author = " + author);
+                System.out.println("isbn = " + isbn);
+                System.out.println("=".repeat(10));
+            }
+            /*rs.next(); // 첫번째 행 지목
+            rs.next(); // 두번째 행 지목
+            rs.next(); // 세번째 행 지목
+*/
         } catch (Exception e) {
             e.printStackTrace();
         }
