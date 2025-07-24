6970196a1f0b11c802fc90a0a5daaa1b04be636b
hsp64 <pphs604@gmail.com>
2025-07-24 09:49:53 +0900

    BookRepository 인터페이스 도입에 따른 레포지토리 구조 개선 및 컨트롤러 수정, Spring JDBC 기반 수정/삭제 메서드 구현 및 테스트 추가

--- src/test/java/com/spring/database/chap01/repository/BookSpringRepositoryTest.java
+++ src/test/java/com/spring/database/chap01/repository/BookSpringRepositoryTest.java
@@ -11,52 +11,53 @@ import static org.junit.jupiter.api.Assertions.*;
 @SpringBootTest
 class BookSpringRepositoryTest {
 
-
     @Autowired
-    private BookSpringRepository bookSpringRepository;
+    BookSpringRepository bookSpringRepository;
 
     @Test
-    @DisplayName("스프링 JDBC로 도서를 생성한다")
+    @DisplayName("스프링 JDBC로 도서를 생성한다.")
     void saveTest() {
         //given
         Book newBook = Book.builder()
                 .title("스프링 JDBC")
                 .author("자바왕")
                 .isbn("S001")
-
                 .build();
         //when
         boolean flag = bookSpringRepository.save(newBook);
         //then
-
+        assertTrue(flag);
     }
 
+
     @Test
-    @DisplayName("Spring jdbc로 수정")
+    @DisplayName("스프링 JDBC로 책 제목과 저자를 수정한다.")
     void updateTest() {
         //given
-        Book updatedBook = Book.builder()
-                .title("수정된 책")
-                .author("수정된 저자명")
-                .id(6L)
+        Book modifyBook = Book.builder()
+                .title("수정된 스프링 책")
+                .author("수정된 자바왕")
+                .id(13L)
                 .build();
         //when
-        boolean flag = bookSpringRepository.updateTitleAndAuthor(updatedBook);
+        boolean b = bookSpringRepository.updateTitleAndAuthor(modifyBook);
+
         //then
-        assertTrue(flag);
+        assertTrue(b);
     }
 
 
     @Test
-    @DisplayName("Spring jdbc로 도서 삭제")
+    @DisplayName("Spring JDBC로 도서 정보 삭제한다.")
     void deleteTest() {
         //given
-        Long givenId = 5L;
+        Long id = 4L;
         //when
-        boolean flag = bookSpringRepository.deleteById(givenId);
+        boolean b = bookSpringRepository.deleteById(id);
         //then
-        assertTrue(flag);
+        assertTrue(b);
     }
 
-}
 
+
+}
